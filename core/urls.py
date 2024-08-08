5d54fb9ee634eb4ea959827ce455dc04ecd0ddd8
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-08 19:05:38 +0000

    chore: register business app and assign an URL path to it

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('business/', include('business.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
