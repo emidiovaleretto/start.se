4b6ee42b0e5d4ef1cdecb80e09b5ed39ae0d0c50
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-11 13:19:38 +0100

    chore: allow only logged-in users to access the business registration page

--- business/views.py
+++ business/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import TIME_EXISTENCE_CHOICES, STAGE_CHOICES, AREA_CHOICES
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/users/signin/')
 def register_business(request):
     if request.method == 'GET':
-        return render(request, 'business/register_business.html')
+        return render(
+            request,
+            'business/register_business.html',
+            {
+                'time_existence_choices': TIME_EXISTENCE_CHOICES,
+                'stage_choices': STAGE_CHOICES,
+                'area_choices': AREA_CHOICES
+            })
