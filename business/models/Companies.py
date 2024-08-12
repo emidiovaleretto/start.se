4f533a5695c0222dde1e380ceaa37d7dddbf70d0
Emidio Valeretto <emidio.valereto@gmail.com>
2024-08-12 18:15:53 +0100

    chore: add get_status Company property

--- business/models/Companies.py
+++ business/models/Companies.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+
+from datetime import date
 from business.models import *
 
 
@@ -20,3 +23,10 @@ class Company(models.Model):
 
     def __str__(self):
         return f"{self.user.username} | {self.name}"
+
+    @property
+    def get_status(self):
+        today = date.today()
+        if today > self.end_capture_date:
+            return mark_safe('<span class="bg-transparent border border-green-200 text-green-200 py-2 px-3 rounded-full text-xs font-bold">Completed</span>')
+        return mark_safe('<span class="bg-transparent border border-yellow-200 text-yellow-200 py-2 px-3 rounded-full text-xs font-bold">In process</span>')
